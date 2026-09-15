33fec19e2127783250fb536785efbe16f3c324fb
ElFernanduz <jffernandez@unillanos.edu.co>
2026-09-15 10:56:02 -0500

    Delete backend directory

--- backend/src/main/java/com/smartparking/repository/ParqueaderoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartparking.repository;
-
-import com.smartparking.model.Parqueadero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParqueaderoRepository extends JpaRepository<Parqueadero, Long> {
-}
